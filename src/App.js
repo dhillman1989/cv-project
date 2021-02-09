4a47d1ad66fc7c1663806f024418e86780c4453e
dave hillman <davehillman@daves-MacBook-Pro.local>
2021-02-09 02:09:55 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "fred",
+      phone: "",
+      email: "",
+      education: [],
+      experience: [],
+    };
+  }
+
+  changeInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.name);
+    console.log(this.state.phone);
+  };
+
+  render() {
+    const { name, phone, email, education, experience } = this.state;
+    return (
+      <Fragment>
+        <input name="name" id="name" value={name} onChange={this.changeInput} />
+        <input
+          name="phone"
+          id="phone"
+          value={phone}
+          onChange={this.changeInput}
+        />
+        <button onClick={this.onSubmit}>Submit</button>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
